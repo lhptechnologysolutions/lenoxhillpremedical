6f3932d2a82c3d31bcdde9e1d3fbc44cf2666212
Phil Murray <philipmurray80@gmail.com>
2023-12-07 09:28:19 -0500

    Initial Commit.

---
+++ src/main/java/com/lhptechnologysolutions/lenoxhillpremedical/LenoxhillpremedicalApplication.java
@@ -0,0 +1,13 @@
+package com.lhptechnologysolutions.lenoxhillpremedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenoxhillpremedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LenoxhillpremedicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhptechnologysolutions/lenoxhillpremedical/LenoxhillpremedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhptechnologysolutions.lenoxhillpremedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenoxhillpremedicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
